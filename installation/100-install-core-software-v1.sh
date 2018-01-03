387b6e289d91d3556779eb490279b0b31e1a462c
Josh <Enigmajdp@gmail.com>
2018-01-03 09:59:40 -0500

    Update 100-install-core-software-v1.sh

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -12,11 +12,11 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
-sudo pacman -S --noconfirm --needed darktable dconf-editor
+sudo pacman -S --noconfirm --needed archey3 baobab clementine conky curl
+sudo pacman -S --noconfirm --needed dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode 
-sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
-sudo pacman -S --noconfirm --needed galculator geary gimp git gksu glances gnome-disk-utility 
+sudo pacman -S --noconfirm --needed evince evolution firefox
+sudo pacman -S --noconfirm --needed galculator gimp git gksu glances gnome-disk-utility 
 sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool 
 sudo pacman -S --noconfirm --needed gparted gpick grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp hexchat htop 
@@ -25,7 +25,7 @@ sudo pacman -S --noconfirm --needed nemo net-tools notify-osd numlockx openshot 
 sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell 
 sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat 
 sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk tumbler
-sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter  
+sudo pacman -S --noconfirm --needed vlc vnstat wget unclutter  
 
 
 sudo systemctl enable vnstat
